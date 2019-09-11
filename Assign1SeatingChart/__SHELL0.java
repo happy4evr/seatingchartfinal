b2105a259dda7f23629a2bb1ad198d26bfdb6cfc
happy4evr <ksaini7152@gmail.com>
2019-09-10 22:38:35 -0700

    Final commit

---
+++ Assign1SeatingChart/__SHELL0.java
@@ -0,0 +1,10 @@
+
+public class __SHELL0 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/khushi/Downloads/Assign1SeatingChart");
+KhushiSaini khushiSa1 = (KhushiSaini)__bluej_runtime_scope.get("khushiSa1");
+
+
+khushiSa1.answerQuestion();
+
+}}
